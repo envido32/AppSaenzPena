f4aa1bd6acc7611229ecf278f2f07fdbd50fda6b
envido32 <envido32@gmail.com>
2017-11-17 19:46:19 -0300

    Open Activity from Preferences

---
+++ app/src/main/java/com/electiva/envido32/appsaenzpena/DistritoActivity.java
@@ -0,0 +1,13 @@
+package com.electiva.envido32.appsaenzpena;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DistritoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_distrito);
+    }
+}
--- app/src/main/java/com/electiva/envido32/appsaenzpena/SettingsActivity.java
+++ app/src/main/java/com/electiva/envido32/appsaenzpena/SettingsActivity.java
@@ -1,5 +1,7 @@
 package com.electiva.envido32.appsaenzpena;
 
+import android.content.Intent;
+import android.preference.Preference;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
